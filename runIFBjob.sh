75f389149283dff47b146405d7b669c8be1e02b5
GomesLucie <c.luciegomes@gmail.com>
2024-12-03 10:16:26 +0100

    Update IFB job script and config file

--- runIFBjob.sh
+++ runIFBjob.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
-#SBATCH --partition=fast
+#SBATCH --partition=long
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --mem-per-cpu=5GB
 #SBATCH -c 1
-#SBATCH -t 23:59:59
 
-
-
-export JAVA_HOME=/shared/software/conda/envs/nextflow-23.10.1
 
 module load nextflow/24.04.1
 
+ROOTDIR="/shared/projects/your_project"
+PROJECTID="exemple"
+INPUT_FQ_DIR=$ROOTDIR"/DUMP_TIPMP/fastq_merged_lanes"
+REZ_DIR=$ROOTDIR"/Results/"$PROJECTID"/"
+REF="/shared/projects/pdacrna/ensembl_v113_GRCh38"
+
 ## Adjust this command by keeping or removing the -resume option
-nextflow -c $2 run $1 -resume -with-report --sampleInputDir $3 --ref $4 --outputdir $5 --sampleList $6
+nextflow -c $2 run $1 -resume -with-report --sampleInputDir $INPUT_FQ_DIR --ref $REF --outputdir $REZ_DIR --sampleList $3
