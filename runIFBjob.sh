e1909e656a101f987613bd701e4dc3709f13053e
GomesLucie <c.luciegomes@gmail.com>
2025-11-03 15:33:15 +0100

    Update config and bash template for IFB process

--- runIFBjob.sh
+++ runIFBjob.sh
@@ -1,16 +1,30 @@
 #!/bin/bash
-#SBATCH --partition=fast
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --mem-per-cpu=5GB
 #SBATCH -c 1
 #SBATCH -t 23:59:59
 
-
-
-export JAVA_HOME=/shared/software/conda/envs/nextflow-23.10.1
-
 module load nextflow/24.04.1
 
-## Adjust this command by keeping or removing the -resume option
-nextflow -c $2 run $1 -resume -with-report --sampleInputDir $3 --ref $4 --outputdir $5 --sampleList $6
+
+### To be customized by user:
+PROJECTID="PROJECT_NAME"
+INPUT_FQ_DIR="/path/to/fastqDir/"
+REZ_DIR="/path/to/resultsDir/"$PROJECTID"/"
+SAMPLE_CSV="/path/to/sample_csv/sample_list.csv"
+CONFIG="/path/to/configFile/IFB_PE_minimal.config"
+
+REF="/path/to/pdacrna/ensembl_v107_GRCh38b_kallisto_v0.51"
+RNAPIPE_DIR="/path/to/RNApipeline/"
+
+### Don't touch variables below this line
+nextflow -c $CONFIG run $RNAPIPE_DIR/RNApipeline.nf -entry Main\
+    -with-report report_$PROJECTID.html -resume \ 
+    --csvSample $SAMPLE_CSV \
+    --ref $REF \
+    --sampleInputDir $INPUT_FQ_DIR \
+    --scriptDir ${RNAPIPE_DIR}"/PrePostScripts/" \
+    --logBackupDir "." \
+    --runNumber $PROJECTID \
+    --outputdir $REZ_DIR \
